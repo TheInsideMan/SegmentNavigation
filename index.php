947f9926eb569190f74a008f113d9b0e37c0d440
Aaron Kenny <akenny@mediamath.com>
2013-12-10 14:46:01 +0000

    some updates to files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript">
+	window.onload = init;
+	function init(){
+		'use strict';
+	}
+	
+
+</script>
+</head>
+<body>
+</body>
+</html>
--- name.php
+++ name.php
@@ -22,6 +22,7 @@
 <body>
 <h1>Aaron was here</h1>
 <h3>some bloody text</h3>
+<p>this is a test for adding text</p>
 	<form method="POST" action="name.php" id="calcForm">
 		<input type="text" name="firstName" id="firstName" value="Firstname" required><br/>
 		<input type="text" name="lastName" id="lastName" value="Lastname" required><br/>
