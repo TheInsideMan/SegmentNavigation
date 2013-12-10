7dc037f5e611b49b501d86774ee94c264940d680
Aaron Kenny <akenny@mediamath.com>
2013-12-10 13:59:01 +0000

    adding name.php

---
+++ name.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript">
+	window.onload = init;
+	function init(){
+		'use strict';
+		document.getElementById('calcForm').onsubmit = formatNames;
+	}
+	function formatNames(){
+		'use strict';
+		var formatedName;
+		var firstName = document.getElementById('firstName').value;
+		var lastName = document.getElementById('lastName').value;
+		formatedName = lastName + ', ' + firstName;
+		document.getElementById('result').value = formatedName;
+		return false;
+	}
+
+</script>
+</head>
+<body>
+
+	<form method="POST" action="name.php" id="calcForm">
+		<input type="text" name="firstName" id="firstName" value="Firstname" required><br/>
+		<input type="text" name="lastName" id="lastName" value="Lastname" required><br/>
+		<input type="text" name="result" id="result" ><br/>
+		<input type="submit" value="Submit" id="submit"> 
+	</form>	
+</body>
+</html>
