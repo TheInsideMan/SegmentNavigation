9decdede4415380c3b34d3077ebff3b16903ff93
Aaron Kenny <akenny@mediamath.com>
2013-12-10 14:48:05 +0000

    changes to h1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript">
+	window.onload = init;
+	function init(){
+		'use strict';
+	}
+	
+
+</script>
+</head>
+<body>
+	<h1>This is a title test</h1>
+</body>
+</html>
